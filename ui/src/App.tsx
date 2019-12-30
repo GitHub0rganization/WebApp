69afe0fed5a40312cc529dd4ef8c552cc57cda2e
ia17011 <cfoyvlird@gmail.com>
2019-12-30 14:20:53 +0900

    Created PrivateRoute component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,17 +4,20 @@ import {Home} from './containers/Home';
 import {Login} from './containers/Login';
 import {User} from './containers/User';
 import {NotFound} from './containers/NotFound';
+import {PrivateRoute} from './containers/PrivateRoute';
 
 const App: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/username" component={User} />
+      <Route path="/username" component={User} />
       <Route exact path="/username/favorites" component={User} />
       <Route exact path="/username/links" component={User} />
       <Route path="*" children={<NotFound />} />
     </Switch>
+    // usage of PrivateRoute forces login
+    // <PrivateRoute path="/hogehoge" component={hoge} />
   );
 };
 
---
+++ ui/src/services/AuthService.ts
@@ -0,0 +1,5 @@
+export const checkAuth = () => {
+  // TODO: check if user login with LocalStorage
+  // TODO: return true untill login function is established
+  return true;
+};
