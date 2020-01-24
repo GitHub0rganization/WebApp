6a9fd721300c0fc12fd16cb18b957f5458a79e25
ia17011 <cfoyvlird@gmail.com>
2020-01-25 02:07:04 +0900

    Update text color

--- ui/src/containers/UsersQRCode.tsx
+++ ui/src/containers/UsersQRCode.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {MDBCard, MDBContainer, MDBCol, MDBRow} from 'mdbreact';
 import QRCode from 'qrcode.react';
+import {Color} from '../utils/variables';
 
 export const UsersQRCode = () => {
   const currentUrl = window.location.href;
@@ -11,7 +12,9 @@ export const UsersQRCode = () => {
         <MDBCol sm="12" className="py-3">
           <MDBRow>
             <MDBCol sm="12">
-              <h2 className="py-2 text-center">Scan!</h2>
+              <h2 className="py-2 text-center" style={{color: Color.BASE}}>
+                Scan!
+              </h2>
               <MDBCard className="card-body p-5 round text-center">
                 <QRCode value={currentUrl} style={{margin: '0 auto'}} />
               </MDBCard>
